1fce3a8aa72b10d9c966506d941eae3152b4b5ed
Claire Durham <clairedurham@Claires-MacBook-Pro.local>
2023-01-26 08:13:33 -0800

    adding styles and category item/directory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
+import CategoryItem from './components/category-item/category-item.component';
+import categories from './components/category-directory/category-directory-menu';
+import './categories.style.scss'
+
 
 const App = () => {
 
-    const categories = [
-      {
-        id: 1,
-        title: 'hats',
-
-      }
-    ]
+   
   return (
     <div className="categories-container">
-      {categories.map(({title}) => (
-      <div className="category-container">
-        {/* {<img />} */}
-        <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-
-        </div>
-      </div>
+      {categories.map((category) => (
+      <CategoryItem key={category.id} category={category} />
     
       ))}
     </div>
